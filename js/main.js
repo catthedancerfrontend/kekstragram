2322d88dd4e43866c9c6e78282c45ec78819689b
catthedancer <catthedancer@hotmail.com>
2022-03-14 22:17:04 +0300

    Добавлены функции утилиты.

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const getRandomNumber = function (from, to) {
+  if (from < 0 || to <= from) {
+    throw new Error('Неверный параметр!');
+  }
+  return Math.round(Math.floor(Math.random() * (Math.floor(to) - Math.ceil(from) + 1) + Math.ceil(from)));
+};
+getRandomNumber(5, 30);
+
+const isValidLength = function (str, maxLength) {
+  return (str.length <= maxLength);
+};
+isValidLength('Неверный параметр!', 100);
