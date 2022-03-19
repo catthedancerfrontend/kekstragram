6d465e81fabea33507c3d1b9b097a5f7f623f887
catthedancer <catthedancer@hotmail.com>
2022-03-19 17:57:58 +0300

    Task 4.9. Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,12 +1,83 @@
-const getRandomNumber = function (from, to) {
-  if (from < 0 || to <= from) {
-    throw new Error('Неверный параметр!');
-  }
-  return Math.floor(Math.random() * (Math.floor(to) - Math.ceil(from) + 1) + Math.ceil(from));
+// const getRandomNumber = function (from, to) {
+//   if (from < 0 || to <= from) {
+//     throw new Error('Неверный параметр!');
+//   }
+//   let lastNumber = getRandomNumber.lastNumber;
+//   let randomNumber = lastNumber;
+//   while (randomNumber == lastNumber) {
+//     randomNumber = Math.floor(Math.random() * (Math.floor(to) - Math.ceil(from) + 1) + Math.ceil(from));
+//   }
+//   getRandomNumber.lastNumber = randomNumber
+//   return randomNumber;
+// };
+
+const getRandomNumber = (min, max) => {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 };
-getRandomNumber(5, 30);
 
 const isValidLength = function (str, maxLength) {
   return str.length <= maxLength;
 };
-isValidLength('Неверный параметр!', 100);
+isValidLength('HELLO', 10);
+
+const DESCRIPTION = [
+  'Если смогу, я сделаю это. Конец истории.',
+  'Я не толстый. Меня просто легче увидеть, чем всех остальных.',
+  'Я не ленивый. Просто у меня нет мотивации.',
+  'Это просто моя жизнь в моем неповторимом стиле.',
+  'Воскресенье — еще один способ сказать: «Какой чудесный день!»',
+  'Предупреждение: на этом фото вы можете влюбиться в меня.',
+  'Предупреждение: мой рот работает быстрее, чем мой мозг.',
+  'Мне нравится думать, что я на диете. Как же это ужасно!',
+  'Всегда есть дикая сторона, которая скрывается за самым невинным лицом.',
+  'Я просто увидел самого умного человека в мире, когда смотрел в зеркало.',
+  'Правило жизни: никогда не проверяйте глубину воды обеими ногами.',
+];
+
+const MESSAGE = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const NAME = [
+  'dominator3000',
+  'hanging_with_my_gnomies',
+  'sand_picture',
+  'averagestudent',
+  'Donald_300',
+  'shaquille_oatmeal',
+  'im_dolphin',
+  'CatBot',
+  'MasterCat',
+  'fast_and_curious.',
+];
+
+const MAX_COMMENTS_NUMBER = 15;
+const OBJECT_COUNT = 25;
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
+
+const getRandomComment = () => ({
+  id: getRandomNumber(1, 200),
+  avatar: `img/avatar-${getRandomNumber(1, 6)}.svg`,
+  message: getRandomArrayElement(MESSAGE),
+  name: getRandomArrayElement(NAME),
+});
+
+const getRandomObject = () => ({
+  id: getRandomNumber(1, 25),
+  url: `photos/${getRandomNumber(1, 25)}.jpg`,
+  description: getRandomArrayElement(DESCRIPTION),
+  likes: getRandomNumber(15, 200),
+  comments: Array.from({length: getRandomNumber(0, MAX_COMMENTS_NUMBER)}, getRandomComment),
+});
+
+const objectArr = Array.from({length: OBJECT_COUNT}, getRandomObject);
+
+console.log(objectArr);
