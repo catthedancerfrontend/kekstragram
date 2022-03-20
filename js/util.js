a5b1aaf3259c355c54c0d56cad33648d37751c95
catthedancer <catthedancer@hotmail.com>
2022-03-20 22:02:12 +0300

    Changed naming of the function getArrayFrom

--- js/util.js
+++ js/util.js
@@ -11,6 +11,6 @@ const isValidLength = function (str, maxLength) {
 
 const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
 
-const getRandomObjectArray = (count, callback) => Array.from({length: count}, callback);
+const getArrayFrom = (count, callback) => Array.from({length: count}, callback);
 
-export {getRandomNumber, isValidLength, getRandomArrayElement, getRandomObjectArray};
+export {getRandomNumber, isValidLength, getRandomArrayElement, getArrayFrom as getRandomObjectArray};
