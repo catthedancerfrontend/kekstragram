26c8f9cb37af128b961f827ca446d214f23d3dc4
catthedancer <catthedancer@hotmail.com>
2022-04-09 13:01:12 +0300

    API in progress

--- js/api.js
+++ js/api.js
@@ -1,21 +1,24 @@
-const createData = 
-fetch(
-  'https://24.javascript.pages.academy/kekstagram/data',
-  {
-    method: 'GET',
-    credentials: 'same-origin',
-  },
-)
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
-    }
+const createData = (onSuccess, onError) => () => {
+  fetch(
+    'https://24.javascript.pages.academy/kekstagram/data',
+    {
+      method: 'GET',
+      credentials: 'same-origin',
+    },
+  )
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
 
-    throw new Error(`${response.status} ${response.statusText}`);
-  })
-  .then((data) => {
-    console.log('Результат', data);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+      throw new Error(`${response.status} ${response.statusText}`);
+    })
+    .then((data) => {
+      console.log('Результат', data);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+export { createData };
